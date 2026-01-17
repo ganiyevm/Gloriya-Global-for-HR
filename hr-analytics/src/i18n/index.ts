e847ed22d89307c3a04be8a3e60734a6742d6bac
ganiyevm <xusniddinganiyev47@gmail.com>
2026-01-17 19:22:41 +0500

    change

--- hr-analytics/src/i18n/index.ts
+++
@@ -1,3 +0,0 @@
-export { LanguageProvider, useLanguage, languageNames, languageFlags } from './LanguageContext';
-export { translations } from './translations';
-export type { Language } from './translations';
